91de1cdffe113da96466449bf847d867ab11cbc2
Pravali <Pravali@node0.pravali-261828.cloudprof-pg0.utah.cloudlab.us>
2025-07-14 22:11:29 -0600

    Update: copied validation-dataset contents to repo root and fixed evaluation scripts 890 updated

--- validation-dataset/benchmarks/benchmark_890/agent_optimized.cpp
+++ validation-dataset/benchmarks/benchmark_890/agent_optimized.cpp
@@ -1,5 +1,8 @@
 #include "hash_common.hpp"
 #include <omp.h>
+#include <unordered_map>
+#include <vector>
+#include <set>
 
 void hash_sequences(std::vector<std::string>& keys,
         std::vector<char*>& seqs,
@@ -12,24 +15,40 @@ void hash_sequences(std::vector<std::string>& keys,
         bool doReadDepth,
         bool doReferenceDepth) {
 
-    #pragma omp parallel for
-    for (int i = 0; i < static_cast<int>(keys.size()); i++) {
-        auto hashes_and_num = allhash_unsorted_64_fast(seqs[i], lengths[i], kmer);
-        hashes[i] = std::get<0>(hashes_and_num);
-        hash_lengths[i] = std::get<1>(hashes_and_num);
-        if (doReadDepth) {
-            for (int j = 0; j < hash_lengths[i]; j++) {
-                #pragma omp atomic
-                read_hash_to_depth[hashes[i][j]]++;
+    #pragma omp parallel
+    {
+        std::unordered_map<hash_t, int> local_read_hash_to_depth;
+        std::unordered_map<hash_t, int> local_ref_to_sample_depth;
+
+        #pragma omp for
+        for (int i = 0; i < static_cast<int>(keys.size()); i++) {
+            auto hashes_and_num = allhash_unsorted_64_fast(seqs[i], lengths[i], kmer);
+            hashes[i] = std::get<0>(hashes_and_num);
+            hash_lengths[i] = std::get<1>(hashes_and_num);
+            if (doReadDepth) {
+                for (int j = 0; j < hash_lengths[i]; j++) {
+                    local_read_hash_to_depth[hashes[i][j]]++;
+                }
+            } else if (doReferenceDepth) {
+                std::set<hash_t> sample_set(hashes[i], hashes[i] + hash_lengths[i]);
+                for (auto x : sample_set) {
+                    local_ref_to_sample_depth[x]++;
+                }
             }
-        } else if (doReferenceDepth) {
-            std::set<hash_t> sample_set(hashes[i], hashes[i] + hash_lengths[i]);
-            for (auto x : sample_set) {
-                #pragma omp atomic
-                ref_to_sample_depth[x]++;
+            delete[] hashes[i];
+        }
+
+        #pragma omp critical
+        {
+            if (doReadDepth) {
+                for (const auto& pair : local_read_hash_to_depth) {
+                    read_hash_to_depth[pair.first] += pair.second;
+                }
+            } else if (doReferenceDepth) {
+                for (const auto& pair : local_ref_to_sample_depth) {
+                    ref_to_sample_depth[pair.first] += pair.second;
+                }
             }
         }
-        delete[] hashes[i];
     }
 }
-
